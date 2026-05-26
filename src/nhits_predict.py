e59e4f33cb09c670e42a51b9757d4a9452e96af7
louisebphansen <louise.bp@hotmail.com>
2026-05-26 13:02:21 +0200

    updated nhits predict script

--- src/nhits_predict.py
+++ src/nhits_predict.py
@@ -1,3 +1,7 @@
+"""
+Forecast on hold-out test set using the best NHITS configuration for each particle
+"""
+
 import numpy as np 
 from neuralforecast.core import NeuralForecast
 from neuralforecast.losses.pytorch import MAE, HuberLoss
@@ -13,6 +17,7 @@ import re
 # set CPU fallback for torch when mps - apple silicon GPUs - are not supported
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
+# define command line argument parser
 def argument_parser():
 
     parser = argparse.ArgumentParser()
@@ -25,7 +30,14 @@ def argument_parser():
     return args
 
 def refit_and_predict(best_config, df, model_name, exg_variables, particle):
-    horizon = 96          # forecast 96 hours ahead
+
+    """
+    Refit NHITS model on the full dataset based on the best model configurations and predicts on the hold-out test set.
+    Saves forecast to folder.
+
+    """
+
+    horizon = 96  # forecast 96 hours ahead
     
     # rename + add cols to fit with neuralforecast expectations
     df = df.rename(columns={'Recorded': 'ds', f'{particle}': 'y'})
@@ -33,26 +45,28 @@ def refit_and_predict(best_config, df, model_name, exg_variables, particle):
 
     particle_df = df[['ds', 'unique_id', 'y'] + exg_variables]
 
+    # divide in train + test dfs
     train_df = particle_df.iloc[:-horizon]
     test_df  = particle_df.iloc[-horizon:]
-
     test_df = test_df[['ds', 'unique_id'] + exg_variables]
 
-    # hacky way of removing MSE() so it's not parsed as a string literal
+    # hacky way of removing MSE() so it's not parsed as a string literal when loading the best model configs..
     best_config = ast.literal_eval(re.sub(r"MSE\(\)", "None", best_config))
     best_config = {k: v for k, v in best_config.items() if k not in {'loss', 'valid_loss', 'h'}}
     
+    # specify model with best configs
     model = NHITS(
         h = horizon,
         **best_config
     )
 
+    # fit and predict
     nf = NeuralForecast(models=[model], freq="h")
     
     nf.fit(train_df)
-
     forecast = nf.predict(futr_df=test_df)
 
+    # save to folder
     forecast_out_path = os.path.join('out', 'forecasts', 'NHITS')
     os.makedirs(forecast_out_path, exist_ok=True)
     forecast.to_csv(os.path.join(forecast_out_path, f"{particle}_{model_name}_forecast.csv"))
@@ -60,16 +74,12 @@ def refit_and_predict(best_config, df, model_name, exg_variables, particle):
 
 def main():
 
+    # load command line args
     args = argument_parser()
 
-    # run script for each particle
-
+    # read data and convert to datetime
     df = pd.read_csv(os.path.join('aarhus_air_quality.csv'))
     df['Recorded'] = pd.to_datetime(df['Recorded'])
-    
-    test_size = 96
-    train_df = df.iloc[:-test_size]
-    test_df  = df.iloc[-test_size:]
         
     exg_variables = args['exg_variables']
 
@@ -79,6 +89,7 @@ def main():
 
     best_config = results_df.query(f"model_type == '{args['model_name']}'")['best_config'].iloc[0]
 
+    # refit model and predict on hold out test set
     refit_and_predict(best_config, df, args['model_name'], exg_variables, args['particle'])
 
 if __name__ == '__main__':
